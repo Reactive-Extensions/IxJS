e0d6c7d23919ffa37a52ba059f987da29d8a4a88
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2013-08-12 12:50:51 -0400

    Fixing interactive assert

--- tests/interactiveassert.js
+++ tests/interactiveassert.js
@@ -1,8 +1,21 @@
-﻿(function (global) {
-    
-    var root = global.Ix;
+﻿(function (root, factory) {
+    var freeExports = typeof exports == 'object' && exports &&
+    (typeof root == 'object' && root && root == root.global && (window = root), exports);
 
-    root.assertionHelper = {
+    // Because of build optimizers
+    if (typeof define === 'function' && define.amd) {
+        define(['Ix', 'exports'], function (Ix, exports) {
+            root.Ix = factory(root, exports, Ix);
+            return root.Ix;
+        });
+    } else if (typeof module == 'object' && module && module.exports == freeExports) {
+        module.exports = factory(root, module.exports, require('ix'));
+    } else {
+        root.Ix = factory(root, {}, root.Ix);
+    }
+}(this, function (global, exp, Ix, undefined) {
+    
+    Ix.assertionHelper = {
         noNext: function (sequence) {
             ok(!sequence.moveNext(), 'Sequence has no next values');
         },
@@ -16,4 +29,5 @@
         }
     };
 
-})(this);
+    return Ix;
+}));
