d9b30050355a6a3a9c4b568cdb9f964b2ef43b43
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-09-29 22:44:24 -0400

    Adding groupBy

--- src/core/linq/groupby.js
+++ src/core/linq/groupby.js
@@ -1,13 +1,25 @@
-  enumerableProto.groupBy = function (keySelector, elementSelector) {
+  /**
+   * Groups the elements of a sequence according to a specified key selector function and creates a result value from each group and its key. 
+   * The elements of each group are projected by using an optional result selector function.
+   * @param {Function} keySelector A function to extract the key for each element.
+   * @param {Function} [elementSelector] A function to map each source element to an element in grouping.
+   * @param {Function} [resultSelector] A function to create a result value from each group.
+   * @returns {Enumerable} A collection of elements where each element represents a projection over a group and its key or the value from the
+   * result selector function.
+   */
+  enumerableProto.groupBy = function (keySelector, elementSelector, resultSelector) {
     if (this == null) {
       throw new TypeError('"this" is null or not defined');
     }
     if (!isFunction(keySelector)) {
       throw new TypeError();
     }
-    if (elementSelector && !isFunction(elementSelector)) {
+    if (!isFunction(elementSelector)) {
       throw new TypeError();
-    }    
+    }
+    if (!isFunction(resultSelector)) {
+      throw new TypeError();
+    }
     var source = this;
     return new Enumerable(function () {
       var m = new Map(), keys = [], it = source[$iterator$](), nextItem;
@@ -23,7 +35,21 @@
       } while (!nextItem.done);
     });
 
+    var len = keys.length, i = 0;
     return new Enumerator(function () {
-
+      if (i < len) {
+        var k = keys[i],
+          values = Observable.from(m.get(k)),
+          val;
+        if (resultSelector) {
+          val = resultSelector(k, values);
+        } else {
+          val = values;
+          val.key = k;
+        }
+        i++;
+        return { done: false, value: val };
+      }
+      return doneIterator;
     });
   };
